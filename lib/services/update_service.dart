277295274ee8c587827dd85ab52712750f052452
synaptic <synaptic_gg@proton.me>
2025-11-22 18:32:49 +0530

    fix : fixed in app updates

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -5,10 +5,15 @@ class UpdateService {
   static Future<void> checkForFlexibleUpdate() async {
     try {
       final updateInfo = await InAppUpdate.checkForUpdate();
-      if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable &&
-          updateInfo.flexibleUpdateAllowed) {
-        await InAppUpdate.startFlexibleUpdate();
-        await InAppUpdate.completeFlexibleUpdate();
+
+      if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable) {
+        final pri = updateInfo.updatePriority;
+        if (pri >= 5) {
+          await InAppUpdate.performImmediateUpdate();
+        } else if (pri >= 3 && updateInfo.flexibleUpdateAllowed) {
+          await InAppUpdate.startFlexibleUpdate();
+          await InAppUpdate.completeFlexibleUpdate();
+        }
       }
     } catch (e) {
       log("Flexible update check failed: $e");
